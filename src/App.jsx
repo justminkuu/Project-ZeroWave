a6a930957b5ae3aef4c8289fa7bc6a1bf6c8535c
Mayank Shahi <crazyorwot0098@gmail.com>
2024-12-27 18:11:37 +0545

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,9 @@ import './styles.css';
 function App() {
   return (
     <div>
-      <head>
-        <title>Project ZeroWave</title>
-        <meta name="description" content="Sustainability and climate change project" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </head>
+      
         <body>
-          <div className="text-3xl text-center text-blue-500">
+          <div className="text-3xl text-center text-red-700">
               ZeroWave
           </div>
         </body>
