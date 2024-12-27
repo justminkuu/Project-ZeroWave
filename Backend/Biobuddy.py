d6902a803b9994d529b61dcf2015ab3176ee8eb8
Gaurav Joshi <codingmeanlogic008@gmail.com>
2024-12-27 23:02:25 +0545

    prompt update

--- Backend/Biobuddy.py
+++ Backend/Biobuddy.py
@@ -19,7 +19,9 @@ def chat():
         """)
     data = request.json
     prompt = data.get('user')
-    result = model.invoke(input=prompt)  # Assuming result contains the bot's response
+    result = model.invoke(input=f"You are a chatbot specializing in topics related to nature, waste management, climate action, and ecosystem sustainability. You will only respond to questions or prompts relevant to these topics. If a question is unrelated to these areas, politely redirect the conversation by saying, 'I can best assist with topics about nature, waste management, or climate action'. Could you ask something along those lines? The question goes like {prompt}"
+
+)  # Assuming result contains the bot's response
     print(result)
     # Use cursor.execute if inserting one row
     cursor.execute("""
