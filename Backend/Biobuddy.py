5fe721d14b4cea1f2250c36945f6192ae5df46b5
Gaurav Joshi <codingmeanlogic008@gmail.com>
2024-12-27 22:51:00 +0545

    Frontend-Backend connect in biobuddy

--- Backend/Biobuddy.py
+++ Backend/Biobuddy.py
@@ -1,22 +1,35 @@
 from langchain_ollama import OllamaLLM
 import sqlite3
+from flask import Flask, request
+from flask_cors import CORS
 
-model = OllamaLLM(model="llama3.2")
+app=Flask(__name__)
+CORS(app)
+@app.route('/', methods=['POST'])
+def chat():  
+    model = OllamaLLM(model="llama3.2")
+    connection = sqlite3.connect("buddy_db.db")
+    cursor = connection.cursor()
+    cursor.execute("""
 
-connection = sqlite3.connect("buddy_db.db")
-cursor = connection.cursor()
-
-while True:
-    user = input("Enter message: ")
-    result = model.invoke(input=user)  # Assuming result contains the bot's response
-    
+        CREATE TABLE IF NOT EXISTS CHATS(
+                   user text not null,
+                   bot text not null
+                   )
+        """)
+    data = request.json
+    prompt = data.get('user')
+    result = model.invoke(input=prompt)  # Assuming result contains the bot's response
+    print(result)
     # Use cursor.execute if inserting one row
     cursor.execute("""
         INSERT INTO CHATS(user, bot)
         VALUES(?,?)
-    """, (user, result))
+    """, (prompt, result))
     
     # Commit changes to the database
     connection.commit()
-    
-    print(result)
+    return ''
+
+if __name__=="__main__":
+    app.run(debug=True)
